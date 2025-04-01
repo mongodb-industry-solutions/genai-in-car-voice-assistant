ad19e54745fb94ae7db23d191846a51f95e9d031
Mike Barnes <michael@journeyapps.com>
2025-04-01 15:12:20 -0600

    Removed development token

--- src/lib/powersync/BackendConnector.js
+++ src/lib/powersync/BackendConnector.js
@@ -14,8 +14,7 @@ export class BackendConnector {
             console.log("Got token", tokenData);
             return {
                 endpoint: this.powersyncUrl,
-                token: "eyJhbGciOiJSUzI1NiIsImtpZCI6InBvd2Vyc3luYy1kZXYtMzIyM2Q0ZTMifQ.eyJzdWIiOiJ0ZXN0IiwiaWF0IjoxNzQzNTM1NTQxLCJpc3MiOiJodHRwczovL3Bvd2Vyc3luYy1hcGkuam91cm5leWFwcHMuY29tIiwiYXVkIjoiaHR0cHM6Ly82N2U0ZDA3OTMwYmU0MTE1ZTBmMDk2ZjcucG93ZXJzeW5jLmpvdXJuZXlhcHBzLmNvbSIsImV4cCI6MTc0MzU3ODc0MX0.vTD8q-KE52hJUgCrFI9JTpdvR251gmBitRlmaq9vazUP1shFptkWXLzWXLp6tRlrnrnLmuAhuIyRJslLs6UfYQQzD4juk0yDApeLjWxHd1vBbizNQjmu6MIoXd0JQVhaHy9UxQOlmbBY9DmTDnGi-4XW1pz2660EL3k3RspAen5QCPl-7lmZj8THM_Xm-1WMMc2HAx76-sA0x1HPXvthey7ZiPnNQMrt4X_5-MDjwsvROlaTLUBEGC1x5Vc8suIZ_-QwQm16lPanRodNrbRq-d0_FSkM1XDGDP7-2Y4DAN302UJeP5U82KUsnScDSm7xdm7Cqub6ozxg9yccRpQyLQ"
-                // token: tokenData.token,
+                token: tokenData.token,
             };
         } catch (error) {
             throw new Error("Failed to fetch token", error);
